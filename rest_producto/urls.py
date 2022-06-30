f549c9c49b2277b03737178caab89123f5bd9803
Thapir <101145680+Thapir@users.noreply.github.com>
2022-06-29 20:56:35 -0400

    Update urls.py

--- rest_producto/urls.py
+++ rest_producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_producto.views import lista_productos, detalle_producto
 
+#Detalle y lista de los productos creados 29/06 20:56
+
 urlpatterns =[
     path('lista_productos', lista_productos, name="lista_productos"),
     path('detalle_producto/<id>', detalle_producto, name="detalle_producto"),
