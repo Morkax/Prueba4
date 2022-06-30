a7f5ee8e6517825b9d7cee063fa8c2541cd4d6a4
Morkax <rexo.navarrete.v@gmail.com>
2022-06-29 20:45:08 -0400

    Prueba numero 4

--- rest_producto/views.py
+++ rest_producto/views.py
@@ -9,7 +9,7 @@ from .serializers import ProductoSerializer
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def lista_productos(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
         producto = Producto.objects.all()
         serializer = ProductoSerializer(producto, many=True)
         return Response(serializer.data)
@@ -21,3 +21,25 @@ def lista_productos(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else: 
             return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalle_producto(request, id):
+    """ Get, Update, o delete de un producto en particular"""
+    try:
+        producto = Producto.objects.get(Id=id)
+    except Producto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProductoSerializer(producto)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request) 
+        serializer = ProductoSerializer(producto, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        producto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
